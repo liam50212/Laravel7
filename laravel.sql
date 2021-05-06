373f0bb22765781eaa779369e5401f372b1be8a3
liam50212 <liam821208@gmail.com>
2021-05-06 17:06:34 +0800

    Update laravel.sql

--- laravel.sql
+++ laravel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2021-05-05 10:49:39
+-- 產生時間： 2021-05-06 11:04:10
 -- 伺服器版本： 10.4.18-MariaDB
 -- PHP 版本： 7.4.16
 
@@ -24,32 +24,102 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- 資料表結構 `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2019_08_19_000000_create_failed_jobs_table', 1),
+(3, '2021_05_06_020355_create_news_table', 1),
+(5, '2021_05_06_031422_remove_tag_from_news_table', 3),
+(9, '2021_05_06_033646_update_tittle_to_news_table', 5);
+
+-- --------------------------------------------------------
+
+--
 -- 資料表結構 `news`
 --
 
 CREATE TABLE `news` (
-  `id` int(11) NOT NULL,
-  `tittle` varchar(255) NOT NULL,
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `tittle` longtext COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `date` date NOT NULL,
-  `img` longtext NOT NULL,
-  `content` longtext NOT NULL,
-  `views` int(10) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `img` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `content` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `view` int(11) NOT NULL DEFAULT 0,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- 傾印資料表的資料 `news`
 --
 
-INSERT INTO `news` (`id`, `tittle`, `date`, `img`, `content`, `views`) VALUES
-(1, 'aaaa', '0000-00-00', 'https://www.taiwan.net.tw/pic.ashx?qp=/0040112/13_0040112_1.jpg&sizetype=2', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 9999),
-(2, 'abc', '2021-05-05', 'https://www.taiwan.net.tw/pic.ashx?qp=/0040115/13_0040115.jpg&sizetype=2', 'asdadasdasdasdasdasd', 0),
-(4, 'abc', '2021-05-05', 'https://www.taiwan.net.tw/pic.ashx?qp=/0040115/13_0040115.jpg&sizetype=2', 'asdadasdasdasdasdasd', 0),
-(5, 'abc', '2021-05-05', 'https://www.taiwan.net.tw/pic.ashx?qp=/0040115/13_0040115.jpg&sizetype=2', 'asdadasdasdasdasdasd', 0),
-(6, 'abc', '2021-05-05', 'https://www.taiwan.net.tw/pic.ashx?qp=/0040115/13_0040115.jpg&sizetype=2', 'asdadasdasdasdasdasd', 0);
+INSERT INTO `news` (`id`, `tittle`, `date`, `img`, `content`, `view`, `created_at`, `updated_at`) VALUES
+(1, '我是標題', '2022-05-06', 'https://www.taiwan.net.tw/pic.ashx?qp=/0040111/13_0040111.jpg&sizetype=2', 'opopopopopopopopopopop', 999, NULL, NULL),
+(2, '王', '2021-07-07', 'https://www.taiwan.net.tw/pic.ashx?qp=/0040115/13_0040115.jpg&sizetype=2', 'yyyyyyyyyyyyyy', 0, NULL, '2021-05-05 22:40:39'),
+(3, '海王', '2021-07-07', 'https://www.taiwan.net.tw/pic.ashx?qp=/0040115/13_0040115.jpg&sizetype=2', 'yyyyyyyyyyyyyy', 0, '2021-05-05 22:10:10', '2021-05-05 22:40:11'),
+(14, 'kkkkkkkkkkkkkk', '2021-07-07', 'https://www.taiwan.net.tw/pic.ashx?qp=/0040115/13_0040115.jpg&sizetype=2', 'yyyyyyyyyyyyyy', 0, '2021-05-05 22:40:06', '2021-05-05 22:40:06'),
+(15, 'oooooo', '2021-05-20', '21212', '12111', 0, '2021-05-05 23:26:25', '2021-05-05 23:26:25'),
+(16, '45431321321', '2021-05-20', '21212', '1321321321321321as32d123a1ds23a1d32as1d321as32d132as1d32asdasdas', 0, '2021-05-05 23:26:46', '2021-05-05 23:26:46');
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- 已傾印資料表的索引
 --
+
+--
+-- 資料表索引 `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- 資料表索引 `news`
@@ -58,14 +128,39 @@ ALTER TABLE `news`
   ADD PRIMARY KEY (`id`);
 
 --
+-- 資料表索引 `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
 -- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
 --
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `news`
 --
 ALTER TABLE `news`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
